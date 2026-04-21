1710487ea5916985e07013426c67d9f739f6d167
devadath-cr_sqex <devadath.cr@innovature.onmicrosoft.com>
2026-04-21 09:49:52 +0530

    changes

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
+import gal
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
